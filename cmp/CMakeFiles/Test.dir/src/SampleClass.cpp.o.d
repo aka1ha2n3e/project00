ecf3d771fddeb5b146226874e5460cb2cddd1dac
髙橋 <165264607+SerenoJourney@users.noreply.github.com>
2024-09-27 08:18:27 +0900

    Delete cmp directory
    Cmakeしか使わず、cmakeでも自動生成されるため。

--- cmp/CMakeFiles/Test.dir/src/SampleClass.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Test.dir/src/SampleClass.cpp.o: \
-  /Users/djredwing/project000/src/SampleClass.cpp \
-  /Users/djredwing/project000/include/SampleClass.hpp
--- cmp/CMakeFiles/TextEditor.dir/src/SampleClass.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/TextEditor.dir/src/SampleClass.cpp.o: \
-  /Users/djredwing/project000/src/SampleClass.cpp \
-  /Users/djredwing/project000/include/SampleClass.hpp
