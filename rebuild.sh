44936fe4582d0612c27a60cf8e5f16b433097d45
sereno <takahashi04111@gmail.com>
2024-10-05 12:46:37 +0900

    shellの作成

--- rebuild.sh
+++ rebuild.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-if [ -d $"build/" ]; then
+if [ -d "build/" ]; then
     rm -r build
 fi
+
 mkdir build
 cd build
-cmake ..
+
+if [ -n "$1" ]; then
+    cmake .. -DMAIN_FILE="$1"
+else
+    cmake ..
+fi
 make
+
+clang-tidy ../src/main.cpp --config-file=../.clang-tidy > tidycheck.txt 2>> tidy-error.txt
+
 cd ..
